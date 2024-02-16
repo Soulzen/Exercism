3954032c00ecb8fa9b5e6441219bdf6b8e00de49
Soulzen <alberto.liria@gmail.com>
2024-02-16 13:35:26 +0100

    knapsack end

--- typescript/knapsack/knapsack.ts
+++ typescript/knapsack/knapsack.ts
@@ -5,10 +5,32 @@ type Item = {
 
 export function maximumValue({
   maximumWeight,
-  items,
+  items
 }: {
   maximumWeight: number
   items: Item[]
 }): number {
-  throw new Error('Remove this statement and implement this function')
+  const dp = new Array(items.length + 1)
+    .fill(0)
+    .map(() => new Array(maximumWeight + 1).fill(0))
+
+  for (let currentItem = 1; currentItem <= items.length; currentItem++) {
+    for (
+      let currentWeight = 0;
+      currentWeight <= maximumWeight;
+      currentWeight++
+    ) {
+      const item = items[currentItem - 1]
+      if (item.weight > currentWeight) {
+        dp[currentItem][currentWeight] = dp[currentItem - 1][currentWeight]
+      } else {
+        dp[currentItem][currentWeight] = Math.max(
+          dp[currentItem - 1][currentWeight],
+          dp[currentItem - 1][currentWeight - item.weight] + item.value
+        )
+      }
+    }
+  }
+
+  return dp[items.length][maximumWeight]
 }
